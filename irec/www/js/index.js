c504bd1cde90d75290fc51ceac7bdb973c0af3e8
Emilio <emilioplatzer@gmail.com>
2015-06-16 11:28:50 -0300

    Estructura vacía de phonegap con jquerymobile

---
+++ irec/www/js/index.js
@@ -0,0 +1,29 @@
+var deviceReady = $.Deferred();
+var documentReady = $.Deferred();
+var jqmReady = $.Deferred();
+console.log('reading main script');
+
+$(document).on('deviceready', function(){
+    console.log('device is ready');
+    deviceReady.resolve();
+});
+$(document).on('mobileinit', function(){
+    console.log('jqm is ready');
+    jqmReady.resolve();
+});
+$(document).ready(function(){
+    console.log('document is ready');
+    documentReady.resolve();
+});
+
+$.when(jqmReady,documentReady, deviceReady).then(init);
+
+function init(){
+  console.log('init');
+  // Configuracion de JQM para phonegap
+  $.mobile.allowCrossDomainPages = true;
+  $.support.cors = true;
+  $.mobile.buttonMarkup.hoverDelay = 0;
+  $.mobile.pushStateEnabled = false;
+  $.mobile.defaultPageTransition = "none";
+}
