8f8560988d5c1172cdbd314cab0d5f7122e26a7f
mar71n <mar71n@gmail.com>
2015-06-30 09:19:44 -0300

    entrevistas.js

---
+++ irec/www/js/entrevistas.js
@@ -0,0 +1,53 @@
+var entrevistas = {
+  lista: [],
+  ready: false,
+  initialize: function(){
+    entrevistas.obtenerEntrevistas(function(err, contents){
+      if(err) {
+        console.log('Error obteniendo el archivo de entrevistas');
+        entrevistas.lista = [];
+      }
+      if(contents) {
+        entrevistas.lista = JSON.parse(contents);
+      }
+      entrevistas.ready = true;
+    });
+  },
+  agregar: function(entrevista, callback) {
+    entrevistas.lista.push(entrevista);
+    entrevistas.guardarEntrevistas(function(){
+      console.log('entrevista agregada y guardada');
+      callback && callback(entrevista);
+    });
+  },
+  guardarEntrevistas: function(callback){
+    var entrevistasEnTexto = JSON.stringify(entrevistas.lista);
+    fileApi.writeTextFile('entrevistas.json', entrevistasEnTexto, function(){
+      callback && callback();
+    });
+  },
+  obtenerEntrevistas: function(callback) {
+    var onError = function(err) {
+      callback && callback(err, null);
+    }
+    var onFile = function(fileEntry) {
+      //convierte el fileEntry en un fileObject
+      fileEntry.file(
+        function(fileObject){
+          var reader = new FileReader();
+          // reader.onerror = function(){
+          //   console.log('reader error');
+          //   console.log(arguments);
+          // }
+          reader.onloadend = function(){
+            // console.log(this.result);
+            callback && callback(null, this.result);
+          }
+          reader.readAsText(fileObject);
+        },
+        onError
+      );
+    }
+    fileApi.dir.getFile('entrevistas.json', {create:true}, onFile, onError);
+  }
+}
