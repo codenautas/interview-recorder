ced463e76dfa9d5945b34a03d9062787ec3e93ce
mar71n <mar71n@gmail.com>
2015-06-30 10:09:43 -0300

    saco fileApi de index.js

--- irec/www/js/index.js
+++ irec/www/js/index.js
@@ -84,51 +84,6 @@ window.addEventListener('error',function(e){
     uglyLog(e.message || ''+e).textContent+=e.stack;
 });
 
-var fileApi = {
-  ready: false, // <--solo por las dudas
-  initialize: function(callback){
-    console.log('fileApi.initializa path: ');
-    var path = cordova.file.dataDirectory;
-    console.log(path);
-    //si no hay problemas, llamamos a callback con el primer
-    //parametro en null (lo que seria el error)
-    var onResolve = function(directoryEntry) {
-      fileApi.dir = directoryEntry;
-      fileApi.ready = true;
-      callback && callback(null, fileApi);
-    }
-    //si hay un error llamamos a callback con el error
-    //como primer parametro (ver arriba)
-    var onError = function(err){
-      callback && callback(err, fileApi);
-    }
-    window.resolveLocalFileSystemURL(path, onResolve, onError);
-  },
-  writeTextFile: function(file, content, callback) {
-    console.log('writeText');
-    var onFile = function(fileEntry) {
-      fileEntry.createWriter(
-        function(fileWriter){
-          fileWriter.write(content);
-          callback && callback(content);
-        }, fileApi.onError);
-    }
-    fileApi.dir.getFile(file, {create: true}, onFile, fileApi.onError);
-  },
-  onError: function(err) {
-    console.log(err);
-  },
-  getDir: function(dir, callback) {
-    var onDir = function(dir){
-      callback && callback(null, dir);
-    }
-    var onError = function(err) {
-      callback && callback(err, null);
-    }
-    fileApi.dir.getDirectory(dir, {create:true}, onDir, onError);
-  }
-};
-
 var guias = {
   lista: [], // <-- array para tener la lista de guias a mano
   ready: false,
