9094610bba13a9da25862662b94055a5a5da4522
estefi-capece <eycapece@gmail.com>
2015-06-30 12:20:29 -0300

    Aplicacion ya funcionando sin el stop y rew 10

---
+++ irec/www/js/helpers.js
@@ -0,0 +1,50 @@
+function crearGuia() {
+  var entrevista = {
+    nombre: 'Curso Phonegap',
+    id: guid(),
+    preguntas: {
+      1: {texto:"Nombre"},
+      2: {texto:"Edad"},
+      3: {texto:"Sexo"},
+      4: {texto:"Estado civil"},
+      5: {texto:"Sobrenombre"}
+    }
+  };
+  return entrevista;
+}
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
+}
+function clockFormat(secs) {
+  secs = secs << 0;
+  var minutes = (secs / 60) << 0;
+  var seconds = secs % 60;
+  minutes = minutes < 10 ? "0"+minutes : minutes;
+  seconds = seconds < 10 ? "0"+seconds : seconds;
+  return minutes+":"+seconds;
+}
+function uglyLog(message){
+    console.log(message);
+    if($){
+        $('[data-role=footer]').append(
+            $('<p>').text(message).addClass('uglylog').click(function(){
+                $('.uglylog').remove();
+            })
+        );
+    }else{
+        var div=document.getElementById('uglyLog');
+        if(!div){
+            div=document.createElement('div');
+            div.id='uglyLog';
+            document.body.appendChild(div);
+        }
+        div.textContent=(div.textContent||'') + message+'. ';
+        return div;
+    }
+}
